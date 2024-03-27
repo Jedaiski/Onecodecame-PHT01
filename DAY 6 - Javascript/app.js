d368200d88f1b704eb5908b3ecba3c6ad18a6eff
Jed Caleb Salayo <jedsalayo@gmail.com>
2024-03-27 19:42:34 +0800

    Added day 6 functions

--- DAY 6 - Javascript/app.js
+++ DAY 6 - Javascript/app.js
@@ -1,87 +1,23 @@
-//Array
+//Functions - Allows us to write reusable blockes of code.
 
-// let fruit = "Mango";
-// console.log(fruit);
-// console.log(typeof(fruit));
-
-let fruits = ["Mango", "Banana", "Apple", "Cherry"];
-console.log(fruits);
-console.log(typeof(fruits));
-//Access elements
-console.log(fruits[2]);
-console.log(fruits[7]); // undefined
-
-// Length of the Index
-console.log(fruits.length);
-
-// Update elements
-fruits[1] = 'Avocado';
-console.log(fruits);
-
-// Adding elements
-// Add element at the end of the array.
-fruits.push("Dewberry");
-console.log(fruits);
-
-// Add element at the beginning of the array.
-fruits.unshift("Banana");
-console.log(fruits);
-
-// Deleting elements
-// Delete element at the end of the array.
-fruits.pop();
-console.log(fruits);
-
-// Delete element at the beginning of the array.
-fruits.shift();
-console.log(fruits);
-
-//Sorting
-// Alphabetically
-fruits.sort();
-console.log(fruits);
-
-//Reverse
-fruits.sort().reverse();
-console.log(fruits);
-
-// Searching
-// indexOf Method
-let index = fruits.indexOf('Avocado');
-console.log("Index:", index);
-
-//Check if an element is present
-//Includes Method
-console.log("Is Apple in fruits: ",fruits.includes("Apple"));
-
-//Joining 
-//Join Method
-console.log("Joined fruits: ", fruits.join("-"));
-
-// Slice Method
-let sliced_fruits = fruits.slice(1, 3);
-console.log(sliced_fruits);
-
-//Slice Method
-// fruits.splice(1, 2);
-// console.log(fruits);
-
-//Looping through the elements 
-fruits.forEach(function(fruit){
-        console.log(fruit);
-});
-
-//For Loop
-for(let i=0; i < fruits.length; i++){
-    console.log(fruits[i]);
+//Defining a function
+function greet(){
+    console.log("Hello World!");
+    console.log("Hello Philippines!");
 }
 
-//Reverse For Loop
-for (let i = fruits.length - 1; i >= 0; i--){
-    console.log(fruits[i]);
+//Calling a function
+greet();
+
+//Function with Parameter
+function greetMe(name) {
+    console.log('Hello, ', name);
+}
+greetMe('Jed');//Argument
+
+//Function with return
+function sum(a,b){
+    return a + b;
 }
 
-//For Of Loop
-for (fruit of fruits){
-    console.log(fruit);
-}
+console.log('Sum', sum(5,8));
